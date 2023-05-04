ad6e89cff06451e73b3a0714cb680dc27f724585
小翁同学 <95159615+kev1nweng@users.noreply.github.com>
2023-05-04 15:09:22 +0800

    preparation for repo visibility change

--- script.js
+++ script.js
@@ -1,60 +1,28 @@
-window.debugMode = false;
-let targetDate = new Date("November 5, 2024 00:00:00").getTime();
+window.debugMode = false; // Manually override using web terminal when debugging
+window.counterShown = false;
 
-setTimeout(() => {
-  eval(
-    atob(
-      "Y29uc29sZS5sb2coIlBsZWFzZSBzdGF5IGFsaXZlLi4uICVjY3V6IHUgYXJlIHRoZSBvbmUuIiwgImNvbG9yOnJnYig1OCwgMTIwLCAyNTUpOyIp"
-    )
-  );
-}, 4000);
+let targetDate = new Date("November 5, 2024 00:00:00").getTime();
 
 // TODO: Base64 quote encryption
 const countdownMsg = [
-  "你一定能行！",
-  "每一天都是新的开始，昨天的那都不是事！",
-  "你的努力一定会有回报。",
-  "不要放弃哦，我在明天等着你。",
-  "只要你愿意，一切皆有可能！",
-  "生活不易，但你可以更坚强。",
-  "每一次的尝试都是一次成长！",
-  "相信自己，你是最棒的！",
-  "永远不要放弃对梦想的追求！",
-  "不要停下前进的步伐，向前走。",
-  "你的坚持会让未来更美好！",
-  "相信自己，你一定可以做到！",
-  "不要让失败打败你，继续向前！",
-  "相信自己，你的能力无限。",
-  "做自己喜欢的事情，让生活更美好！",
-  "永远不要放弃对未来的憧憬！",
-  "不要被困难击倒，坚持到底！",
-  "不要停下脚步，继续前行！",
-  "困难是暂时的，坚持是永恒的！",
-  "相信自己，你的能力无限。",
-  "做自己的主人，掌握自己的命运。",
-  "不要在乎别人的眼光，坚持自己就可以了！",
-  "坚持不懈，永不放弃。",
-  "没有什么事情是不可能的，只要你敢去尝试。",
-  "不要让过去的阴影遮住了现在的阳光。",
-  "相信自己，你拥有改变世界的力量。",
-  "这个世界上，还有很多人爱着你。",
-  "👍加油~",
-  "真心希望你可以健康快乐地活着鸭！",
-  "碰上什么破事，过了 24 小时还不得到明天吗",
-  "还真就和这世界杠上了！",
-  "你是个独一无二的、很有意思的人！",
+  "You are the best!",
+  "Always embrace hope...",
+  "Tomorrow will always be a brand new day.",
 ];
 
 if (debugMode) {
-  countdownMsg = [];
+  countdownMsg = [
+    "",
+    // Debug msg here
+  ];
 }
 
 const countupMsg = [];
 
 // Special text in the last minute
-const messages = [`Text1<br>Text2<br>Text3`];
-
-window.counterShown = false;
+const lastMinuteMsg = [
+  [0, 1000, "A message shown from 0ms to 1000ms after triggering"], // Not final data structure
+];
 
 function countDown() {
   let now = new Date().getTime();
@@ -78,14 +46,16 @@ function countDown() {
     }, 1000);
     counterShown = true;
   }
-  // 缓存 DOM
+  // DOM cache: results in better proformance in loops
   const countdownElement = document.getElementById("countdown");
   countdownElement.innerHTML = formattedTime;
   if (remainingTime <= 0) {
-    countUp();
+    // TODO: countUp(); Not working yet!!
+    // countUp();
     return;
   }
   requestAnimationFrame(countDown);
+  // How to cancel requestAnimationFrame(countDown); when switching to count up?
 }
 
 // TODO
@@ -105,16 +75,18 @@ function countUp() {
   let minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
   let formattedTime = `${years}<b>年</b> ${months}<b>月</b> ${days}<b>日</b><br>${hours}<b>小时</b> ${minutes}<b>分钟</b> ${seconds}<b>秒</b>`;
-  // 缓存 DOM
+  // DOM cache: results in better proformance in loops
   const countdownElement = document.getElementById("countdown");
   countdownElement.innerHTML = formattedTime;
   requestAnimationFrame(countUp);
 }
 
 // TODO
-function theLastMinute(name) {return name;}
+function theLastMinute(name) {
+  return name;
+}
 
-/* online quote
+/* Abandoned: Online quote (hitokoto)
 function fetchQuote() {
   fetch('https://v1.hitokoto.cn/?c=k')
   .then(response => response.json())
@@ -130,22 +102,25 @@ function fetchQuote() {
 function fetchQuote(isCountdown = true) {
   let gotQuote;
   if (isCountdown) {
-    gotQuote =
-      countdownMsg[Math.floor(Math.random() * countdownMsg.length)];
-  } else
-    gotQuote = countupMsg[Math.floor(Math.random() * countupMsg.length)];
+    gotQuote = countdownMsg[Math.floor(Math.random() * countdownMsg.length)];
+  } else gotQuote = countupMsg[Math.floor(Math.random() * countupMsg.length)];
   const quoteQuery = document.querySelector("#quote");
   quoteQuery.innerText = gotQuote;
-  setTimeout(() => {
-    document.getElementById("quote").classList.add("visible");
-  }, 2000);
 }
 
 function initialize() {
   countDown();
   fetchQuote();
+  setTimeout(() => {
+    document.getElementById("quote").classList.add("visible");
+  }, 2000);
+  setTimeout(() => {
+    eval(
+      atob(
+        "Y29uc29sZS5sb2coIlBsZWFzZSBzdGF5IGFsaXZlLi4uICVjY3V6IHUgYXJlIHRoZSBvbmUuIiwgImNvbG9yOnJnYig1OCwgMTIwLCAyNTUpOyIp"
+      )
+    );
+  }, 4000);
 }
 
 initialize();
-
-// odvDZ288Fxg24x1HsWLtdVVxiPHg1W5huaX3ZHnsx
--- star.js
+++ star.js
@@ -10,6 +10,7 @@ function resizeCanvas() {
 }
 
 function generateStars() {
+  // Initializing the array every time in case of issues when regenerating stars
   stars = [];
   for (let i = 0; i < numStars; i++) {
     stars.push({
@@ -33,14 +34,13 @@ canvas.height = window.innerHeight;
 generateStars();
 var alphaFrozen = false;
 
-// 绘制星点
 function drawStars() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < numStars; i++) {
     let star = stars[i];
-    let baseAlpha = 0.4;
+    let baseAlpha = 0.3;
     if (!alphaFrozen) {
-      alpha = baseAlpha + Math.random() * 0.3;
+      alpha = baseAlpha + Math.random() * 0.2;
     } else {
       alpha = star.alpha;
     }
@@ -49,11 +49,15 @@ function drawStars() {
     }
     if (brightShown) {
       // star.x += star.vx;
+      /* 
+      Removing this line cuz randomized x-axis movements can cause 
+      a reduction in the number of stars in long sessions
+      */
       star.y -= star.vy;
       if (i % 2 == 1) {
-        star.x += Math.sin((Date.now() * star.shakeRate) / 4) * 2; // 正弦函数平滑移动
+        star.x += Math.sin((Date.now() * star.shakeRate) / 4) * 2; // Movement smoothing
       } else {
-        star.x -= Math.sin((Date.now() * star.shakeRate) / 4) * 2; // 正弦函数平滑移动
+        star.x -= Math.sin((Date.now() * star.shakeRate) / 4) * 2; // Movement smoothing
       }
       star.y -= 5;
       /* // TODO: X axis edge detection
@@ -75,7 +79,7 @@ function drawStars() {
       ctx.arc(star.x, star.y, star.radius * 2, 0, 2 * Math.PI);
     } else {
       alphaFrozen = false;
-      /* // Legacy
+      /* // Legacy: stars without glow effect
       ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
       ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
       */
@@ -95,23 +99,19 @@ function drawStars() {
       ctx.fill();
       ctx.beginPath();
       ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
-      ctx.arc(
-        star.x,
-        star.y,
-        star.radius * 0.5,
-        0,
-        Math.PI * 2
-      );
+      ctx.arc(star.x, star.y, star.radius * 0.5, 0, Math.PI * 2);
       ctx.fill();
     }
     ctx.fill();
   }
 }
+
 let fps = 12;
 let then = Date.now();
+
 function animate() {
   requestAnimationFrame(animate);
-  let interval = 1000 / fps; // 计算每帧间隔时间
+  let interval = 1000 / fps;
   if (brightShown) {
     fps = 30;
   } else {
@@ -120,9 +120,8 @@ function animate() {
   const now = Date.now();
   const delta = now - then;
   if (delta > interval) {
-    // 如果时间间隔达到限定帧率
-    then = now - (delta % interval); // 更新时间标记
-    drawStars(); // 进行绘制
+    then = now - (delta % interval);
+    drawStars();
   }
   if (!starShown) {
     setTimeout(() => {
@@ -131,4 +130,5 @@ function animate() {
     starShown = true;
   }
 }
+
 animate();
